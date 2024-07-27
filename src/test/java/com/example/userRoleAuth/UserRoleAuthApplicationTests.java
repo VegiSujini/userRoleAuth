fa9a8e6d023f5518a9218b21251da0fa083ae3e6
VegiSujini <sujini.vegi@gmail.com>
2024-07-27 16:25:06 +0530

    initial commit

---
+++ src/test/java/com/example/userRoleAuth/UserRoleAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userRoleAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRoleAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
